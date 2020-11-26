c6a69d70a87c8bb3c70be806cb4192815f0c75f8
Pooja <poojavpatel71@gmail.com>
2020-11-27 02:09:10 +0530

    added vuetify to proj, cleaned repo, added axios lodash

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import vuetify from '@/plugins/vuetify';
 import App from './App';
 
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
+  vuetify,
   el: '#app',
   components: { App },
   template: '<App/>',
